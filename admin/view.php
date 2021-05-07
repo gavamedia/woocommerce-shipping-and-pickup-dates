3659001ddf3870980b3bcf65d9cb4d6d3b65aa28
GAVAMEDIA <chrisringrose@gavamedia.com>
2021-05-07 17:37:11 -0230

    Duh

--- admin/view.php
+++ admin/view.php
@@ -28,7 +28,7 @@ $html_template = file_get_contents(plugin_dir_path( __FILE__ ) . '/html/index.ht
 
 
 // Add notices
-$noticeHTML = '';
+$noticeHTML = 'You have no shipping or pickup dates enabled';
 if ($noticeHTML) {
 	$noticeHTML = <<<GAVA
 	<div class="notice js-wc-plugin-framework-admin-notice error is-dismissible" data-plugin-id="bambora" data-message-id="bambora_credit_card-not-configured">
