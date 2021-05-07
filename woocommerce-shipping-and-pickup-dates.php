fc9d031af20fe122b0057d22b5aae0c8ed17068e
GAVAMEDIA <chrisringrose@gavamedia.com>
2021-05-07 21:02:19 -0230

    test

--- woocommerce-shipping-and-pickup-dates.php
+++ woocommerce-shipping-and-pickup-dates.php
@@ -29,26 +29,32 @@
 
 defined('ABSPATH') or die('No script kiddies, please!');
 //define('WP_DEBUG', true);
+
+
+
+
+
+
+if (!function_exists('str_starts_with')) {
+	function str_starts_with($haystack, $needle) {
+		return (string)$needle !== '' && strncmp($haystack, $needle, strlen($needle)) === 0;
+	}
+}
+if (!function_exists('str_ends_with')) {
+	function str_ends_with($haystack, $needle) {
+		return $needle !== '' && substr($haystack, -strlen($needle)) === (string)$needle;
+	}
+}
+if (!function_exists('str_contains')) {
+	function str_contains($haystack, $needle) {
+		return $needle !== '' && mb_strpos($haystack, $needle) !== false;
+	}
+}
+
+
+
 if (!class_exists('WSAPD_Plugin')) {
 	class WSAPD_Plugin {
-
-		if (!function_exists('str_starts_with')) {
-			function str_starts_with($haystack, $needle) {
-				return (string)$needle !== '' && strncmp($haystack, $needle, strlen($needle)) === 0;
-			}
-		}
-		if (!function_exists('str_ends_with')) {
-			function str_ends_with($haystack, $needle) {
-				return $needle !== '' && substr($haystack, -strlen($needle)) === (string)$needle;
-			}
-		}
-		if (!function_exists('str_contains')) {
-			function str_contains($haystack, $needle) {
-				return $needle !== '' && mb_strpos($haystack, $needle) !== false;
-			}
-		}
-
-
 
 
 
