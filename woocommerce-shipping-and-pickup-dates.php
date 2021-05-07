15656048008fb4963aec0fcf3806e3d6859696df
GAVAMEDIA <chrisringrose@gavamedia.com>
2021-05-07 20:02:06 -0230

    Js fix

--- woocommerce-shipping-and-pickup-dates.php
+++ woocommerce-shipping-and-pickup-dates.php
@@ -37,8 +37,8 @@ defined('ABSPATH') or die('No script kiddies, please!');
 define('WSAPD_CSS_FILE_PATH', plugin_dir_path( __FILE__ ) . 'admin/style.css');
 define('WSAPD_CSS_URL', plugins_url('admin/style.css', __FILE__));
 
-define('WSAPD_JS_FILE_PATH', plugin_dir_path( __FILE__ ) . 'admin/menus.js');
-define('WSAPD_JS_URL', plugins_url('admin/menus.js', __FILE__));
+define('WSAPD_JS_FILE_PATH', plugin_dir_path( __FILE__ ) . 'admin/script.js');
+define('WSAPD_JS_URL', plugins_url('admin/script.js', __FILE__));
 
 
 
