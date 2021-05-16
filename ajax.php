82a432e697d6234eb82a4b3778f37784673afb9c
GAVAMEDIA <chrisringrose@gavamedia.com>
2021-05-15 21:54:08 -0230

    Test

--- ajax.php
+++ ajax.php
@@ -1,23 +1,28 @@
 <?php
 
 
+if (!class_exists('WSAPD_Plugin')) {
+	class WSAPD_Plugin {
 
-function example_ajax_request() {
+		function example_ajax_request() {
 
-	// The $_REQUEST contains all the data sent via AJAX from the Javascript call
-	if ( isset($_REQUEST) ) {
+			// The $_REQUEST contains all the data sent via AJAX from the Javascript call
+			if ( isset($_REQUEST) ) {
 
-			$fruit = $_REQUEST['fruit'];
+					$fruit = $_REQUEST['fruit'];
 
-			// This bit is going to process our fruit variable into an Apple
-			if ( $fruit == 'Banana' ) {
-					$fruit = 'Apple!!!';
+					// This bit is going to process our fruit variable into an Apple
+					if ( $fruit == 'Banana' ) {
+							$fruit = 'Apple pen?';
+					}
+
+					// Now let's return the result to the Javascript function (The Callback) 
+					echo $fruit;        
 			}
 
-			// Now let's return the result to the Javascript function (The Callback) 
-			echo $fruit;        
-	}
+			// Always die in functions echoing AJAX content
+			die();
+		}
 
-	// Always die in functions echoing AJAX content
-	die();
+	}
 }
