e177b13b998c12e3056e86903c4c456d50077233
GAVAMEDIA <chrisringrose@gavamedia.com>
2021-05-15 22:27:57 -0230

    jQuery fixes

--- admin/script.js
+++ admin/script.js
@@ -23,145 +23,6 @@
 
 
 
-// Easily add events to elements
-if (!Element.prototype.AddEvent)
-	Element.prototype.AddEvent = function(eventName, callFunction) {
-		if (this.addEventListener) this.addEventListener(eventName.substring(2), callFunction, true);
-  	else if (this.attachEvent) this.attachEvent(eventName, callFunction);
-	};
-if (!Element.prototype.RemoveEvent)
-	Element.prototype.RemoveEvent = function(eventName, callFunction) {
-		if (this.removeEventListener) this.removeEventListener(eventName.substring(2), callFunction, true);
-  	else if (this.DetachEvent) this.DetachEvent(eventName, callFunction);
-	};
-
-
-
-
-
-
-// Easily handle AJAX
-if (typeof runningAjaxCalls === 'undefined') {
-
-	let runningAjaxCalls = [];
-
-
-
-
-	function gavaAjaxCallOnReadyStateChange(e) {
-
-		let eReadyState = e.readyState;
-		let thisReadyState = this.readyState;
-
-		
-		if (runningAjaxCalls[callID].readyState == 4) {
-				
-			// Include unique ajax call ID back to return return functions
-			runningAjaxCalls[callID]['CallID'] = callID;
-
-			
-			if (runningAjaxCalls[callID].status == 401) {
-				if (typeof window.LoginRequired == 'function') {
-					CallAfterLoginFunctionID = FunctionID;
-					CallAfterLoginParameters = Parameters;
-					CallAfterLoginReturnFunction = ReturnFunction;
-					LoginRequired();
-				}
-				else ShowDialog('<h2>Login Required</h2><br>Please login to continue.', 'loginRequiredResponseBackup', 330, 145, false);
-			}
-			else if (runningAjaxCalls[callID].status == 403) {
-				ShowDialog('<h2>Access Denied</h2><br>You do not have permission to perform this action.', null, 330, 145, false);
-				setTimeout(function(){if (typeof ReturnFunction == 'function') ReturnFunction(runningAjaxCalls[callID]);}, 400);
-			}
-			else {
-				if (typeof ReturnFunction == 'function') ReturnFunction(runningAjaxCalls[callID]);
-			}
-			
-			
-			// DONE. Remove from running list
-			delete runningAjaxCalls[callID];
-		}
-	}
-
-
-
-	function gavaMaybeAbortThisOrOtherAjaxCall(callID, cancelID) {
-		let abortThis = false;
-
-		// Already running
-		let runningAjaxObj = null;
-		try { runningAjaxObj = runningAjaxCalls[callID]; } catch(x){}
-
-		if (runningAjaxObj) {
-			// Cancel others, but run this
-			if (cancelID) runningAjaxCalls[callID].abort();
-
-			// Or cancel this
-			else abortThis = true;
-		}
-
-		return abortThis;
-	}
-
-
-
-
-
-
-	/**
-	 * Calls an app's server-side function
-	 * If cancelID is set, it will cancel other pending calls with the same id,
-	 * even though parameters may be different
-	 */
-	function gavaAjax(serverFunctionName, parameters, returnFunction, cancelID) {
-
-		let callID = null;
-		if (cancelID) callID = encodeURIComponent(cancelID);
-		else callID = encodeURIComponent(serverFunctionName + parameters + returnFunction);
-		
-
-		// Already running. Cancel others and run this, or cancel this
-		if (gavaMaybeAbortThisOrOtherAjaxCall(callID, cancelID)) return false;
-
-
-		// Create the ajax obj
-		runningAjaxCalls[callID] = new XMLHttpRequest();
-
-
-		
-		// Prepare all parameters, and prevent cached response
-		let params = 'action=' + serverFunctionName;
-		if (parameters) params += '&' + parameters;
-		
-
-		// Setup ajax header
-		runningAjaxCalls[callID].open('POST', ajaxurl, true);
-		runningAjaxCalls[callID].setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-
-		// Execute return function when ready
-		runningAjaxCalls[callID].onreadystatechange = gavaAjaxCallOnReadyStateChange;
-		
-		
-
-		
-		// Submit the AJAX request
-		runningAjaxCalls[callID].send(params + '&u=' + Math.random());
-		
-		
-		// Return unique call ID
-		return callID;
-	}
-
-
-}
-
-
-
-
-
-
-
 
 function toggleAreaViaChecked(isChecked, areaElm, styleToChange, styleValueIfChecked, styleValueIfUnChecked) {
 	areaElm.style[styleToChange] = isChecked ? styleValueIfChecked : styleValueIfUnChecked;
@@ -183,17 +44,15 @@ function wsapdSave() {
 	//$('#wsapd-enable-shipping-dates')
 
 
-	let newSettings = {
-		'action': 'example_ajax_request',
-
-		'enable-shipping-dates': $('#wsapd-enable-shipping-dates').checked
-	};
-
 	
 	// This does the ajax request (The Call).
 	$.ajax({
 		url: ajaxurl, // admin-ajax.php
-		data: newSettings,
+		data: {
+			'action': 'example_ajax_request',
+	
+			'enable-shipping-dates': $('#wsapd-enable-shipping-dates').checked
+		},
 		success: function(data) {
 			// This outputs the result of the ajax request (The Callback)
 			window.alert(data);
@@ -227,12 +86,12 @@ jQuery(document).ready(function($jQuery) {
 
 
 	let enableShippingDatesElm = $('#wsapd-enable-shipping-dates');
-	if (enableShippingDatesElm) enableShippingDatesElm.AddEvent('onchange', function() {
+	if (enableShippingDatesElm) enableShippingDatesElm.change(function() {
 		toggleAreaViaChecked(enableShippingDatesElm.checked, $('#wsapd-shipping-dates-area'), 'opacity', '1', '.5');
 	});
 
 	let enablePickupDatesElm = $('#wsapd-enable-pickup-dates');
-	if (enablePickupDatesElm) enablePickupDatesElm.AddEvent('onchange', function() {
+	if (enablePickupDatesElm) enablePickupDatesElm.change(function() {
 		toggleAreaViaChecked(enablePickupDatesElm.checked, $('#wsapd-pickup-dates-area'), 'opacity', '1', '.5');
 	});
 
@@ -246,7 +105,7 @@ jQuery(document).ready(function($jQuery) {
 	toggleAreaViaChecked(enablePickupDatesElm.checked, $('#wsapd-pickup-dates-area'), 'opacity', '1', '.5');
 
 
-	$('#wsapd-save-button').AddEvent('onclick', wsapdSave);
+	$('#wsapd-save-button').click(wsapdSave);
 	
 });
 
